1d3834dde855328a36a508b70a30f21ba0bd866b
kumaresh <kumareshkumareee75@gmail.com>
2022-09-22 16:42:22 +0530

    hks

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -2,11 +2,9 @@ package org.test;
 
 public class Sample {
 
-	private void m1() {
-		System.out.println(21);
-		System.out.println(1221);
-	}
-	private void m2() {
-		System.out.println("e22as");
+	
+	private void m3() {
+		// TODO Auto-generated method stub
+
 	}
 }
